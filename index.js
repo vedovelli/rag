2a81eeb8e2880fed7a146edb3017b3657ac48313
Fábio Vedovelli <vedovelli@gmail.com>
2025-05-17 06:34:02 -0300

    Chunks gerados com sucesso 📝

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ import "dotenv/config";
 import * as lib from "./lib.js";
 
 import FirecrawlApp from "@mendable/firecrawl-js";
-
-const firecrawl = new FirecrawlApp({
-  apiKey: process.env.FIRECRAWL_API_KEY,
-});
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
 const urls = [
+  "https://www.rocketseat.com.br/blog/artigos/post/cats",
   "https://www.rocketseat.com.br/blog/artigos/post/componentes-reutilizaveis-react-2025",
   "https://www.rocketseat.com.br/blog/artigos/post/ingles-lideranca-tecnica-global",
   "https://www.rocketseat.com.br/blog/artigos/post/automatize-tarefas-n8n-docker",
@@ -21,13 +19,32 @@ const urls = [
   "https://www.rocketseat.com.br/blog/artigos/post/profissional-360-acelerar-desenvolvimento",
 ];
 
-// urls.forEach(async (url) => {
-//   const result = await firecrawl.scrapeUrl(url, {
-//     formats: ["markdown"],
-//   });
+const markdownContents = urls.map(lib.extractFileNameFromUrl).map(lib.readFile);
 
-//   if (result.success) {
-//     const fileName = lib.extractFileNameFromUrl(url);
-//     lib.saveFile(fileName, result.markdown ?? "");
-//   }
-// });
+async function chunkContents(markdownContents) {
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 768,
+    chunkOverlap: 80,
+  });
+
+  return await Promise.all(
+    markdownContents.map((markdown) => splitter.splitText(markdown))
+  );
+}
+
+function getContents(urls) {
+  const firecrawl = new FirecrawlApp({
+    apiKey: process.env.FIRECRAWL_API_KEY,
+  });
+
+  urls.forEach(async (url) => {
+    const result = await firecrawl.scrapeUrl(url, {
+      formats: ["markdown"],
+    });
+
+    if (result.success) {
+      const fileName = lib.extractFileNameFromUrl(url);
+      lib.saveFile(fileName, result.markdown ?? "");
+    }
+  });
+}
