371383bdbcd2c669ece9a51e1a07e3f80e5e68d3
Fábio Vedovelli <vedovelli@gmail.com>
2025-05-17 09:19:37 -0300

    Revert "Chunks gerados com sucesso 📝"
    This reverts commit 2a81eeb8e2880fed7a146edb3017b3657ac48313.

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import "dotenv/config";
 import * as lib from "./lib.js";
 
 import FirecrawlApp from "@mendable/firecrawl-js";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
+const firecrawl = new FirecrawlApp({
+  apiKey: process.env.FIRECRAWL_API_KEY,
+});
 
 const urls = [
-  "https://www.rocketseat.com.br/blog/artigos/post/cats",
   "https://www.rocketseat.com.br/blog/artigos/post/componentes-reutilizaveis-react-2025",
   "https://www.rocketseat.com.br/blog/artigos/post/ingles-lideranca-tecnica-global",
   "https://www.rocketseat.com.br/blog/artigos/post/automatize-tarefas-n8n-docker",
@@ -19,32 +21,13 @@ const urls = [
   "https://www.rocketseat.com.br/blog/artigos/post/profissional-360-acelerar-desenvolvimento",
 ];
 
-const markdownContents = urls.map(lib.extractFileNameFromUrl).map(lib.readFile);
+// urls.forEach(async (url) => {
+//   const result = await firecrawl.scrapeUrl(url, {
+//     formats: ["markdown"],
+//   });
 
-async function chunkContents(markdownContents) {
-  const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 768,
-    chunkOverlap: 80,
-  });
-
-  return await Promise.all(
-    markdownContents.map((markdown) => splitter.splitText(markdown))
-  );
-}
-
-function getContents(urls) {
-  const firecrawl = new FirecrawlApp({
-    apiKey: process.env.FIRECRAWL_API_KEY,
-  });
-
-  urls.forEach(async (url) => {
-    const result = await firecrawl.scrapeUrl(url, {
-      formats: ["markdown"],
-    });
-
-    if (result.success) {
-      const fileName = lib.extractFileNameFromUrl(url);
-      lib.saveFile(fileName, result.markdown ?? "");
-    }
-  });
-}
+//   if (result.success) {
+//     const fileName = lib.extractFileNameFromUrl(url);
+//     lib.saveFile(fileName, result.markdown ?? "");
+//   }
+// });
